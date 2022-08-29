aee316fd2a4a235dd891976fa5b0d4bd89be5102
OmshreeS <omshreeshetty1@gmail.com>
2022-08-29 20:50:27 +0530

    iam grp

--- iamgrp.tf
+++ iamgrp.tf
@@ -2,6 +2,7 @@
 resource "aws_iam_user" "user1" {
   name = "User1"
 }
+
 resource "aws_iam_user" "user2" {
   name = "User2"
 }
@@ -9,6 +10,7 @@ resource "aws_iam_user" "user2" {
 resource "aws_iam_group" "grp" {
   name = "demo-group"
 }
+
 resource "aws_iam_policy_attachment" "admin-attach" {
   name = "admin-attach"
   groups = [ aws_iam_group.grp.name ]
@@ -21,3 +23,4 @@ resource "aws_iam_group_membership" "grp" {
   aws_iam_user.user2.name ]
 group = aws_iam_group.grp.name
 }
+
